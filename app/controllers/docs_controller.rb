a05850bfc8b3b8b4fa9008bbf352f2c39cb7d683
sheetalsharmazehntech <sheetal.sharma@zehntech.com>
2022-10-06 18:27:18 +0530

    added singn in page

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -12,7 +12,7 @@ class DocsController < ApplicationController
   end 
 
   def new
-    @doc = Doc.new
+    @doc = current_user.docs.build
   end
 
   def destroy
@@ -21,7 +21,7 @@ class DocsController < ApplicationController
   end
 
   def create
-    @doc = Doc.new(doc_params)
+    @doc = current_user.docs.build(doc_params)
     if @doc.save
       redirect_to @doc
     else
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_06_121818) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_06_122820) do
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
